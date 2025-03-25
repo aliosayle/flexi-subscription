a23d066db468cced7d251ed963dd711986d1d0cf
aliosayle <aliossaily24@gmail.com>
2025-03-25 09:54:47 +0100

    updated server cors configuration

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const authLimiter = rateLimit({
 
 // CORS configuration with stricter options
 app.use(cors({
-  origin: ['http://192.168.10.70:8080', 'http://localhost:8080', 'http://localhost:5173'],
+  origin: true, // Allow all origins in development
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
